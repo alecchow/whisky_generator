3e45b739ef6a863a90ba93a1e7539fe05af39b5a
alecchow <84516588+alecchow@users.noreply.github.com>
2021-05-24 18:20:06 +0100

    Add database

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('Testing');
+//console.log('Testing');
+
+// 3-parts database in nested array
+const database = {
+    label: ['Macallan', 'Balvenie', 'Glenfiddich'],
+    year: ['12', '15', '18'],
+    case: ['Sherry Oak Case', 'Double Cases', 'Triple Cases']
+};
