b69d83b2aec2486c9582b36f57a7bb8ead270d89
alecchow <84516588+alecchow@users.noreply.github.com>
2021-05-24 18:26:48 +0100

    Add loop

--- script.js
+++ script.js
@@ -3,9 +3,29 @@
 //3-parts database in nested array
 const database = {
     label: ['Macallan', 'Balvenie', 'Glenfiddich'],
-    year: ['12', '15', '18'],
+    age: ['12', '15', '18'],
     case: ['Sherry Oak Case', 'Double Cases', 'Triple Cases']
 };
 
 //Random index generator
 const randIndex = Math.floor(Math.random() * 3);
+
+//Empty array to collect the 3-parts message
+let whiskyChoice = [];
+
+//Loop through different component of the message and generate random data from each part
+for (let comp in database) {
+    switch(comp) {
+        case 'label':
+            whiskyChoice.push(`Your drink for tonight is a ${database[comp][randIndex]} `);
+            break;
+        case 'age':
+            whiskyChoice.push(`${database[comp][randIndex]} Years Old `);
+            break;
+        case 'case':
+            whiskyChoice.push(`${database[comp][randIndex]}. Enjoy!`);
+            break;
+        default:
+            whiskyChoice.push('No whisky for you tonight!');
+    }
+};
