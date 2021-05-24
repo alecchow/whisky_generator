f82a27753f3dbf6e9682b01726c964b9ef987152
alecchow <84516588+alecchow@users.noreply.github.com>
2021-05-24 18:22:27 +0100

    Add index generator

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 //console.log('Testing');
 
-// 3-parts database in nested array
+//3-parts database in nested array
 const database = {
     label: ['Macallan', 'Balvenie', 'Glenfiddich'],
     year: ['12', '15', '18'],
     case: ['Sherry Oak Case', 'Double Cases', 'Triple Cases']
 };
+
+//Random index generator
+const randIndex = Math.floor(Math.random() * 3);
